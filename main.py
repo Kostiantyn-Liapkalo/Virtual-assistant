322bdd7d2e422d55f192a82d3baca4fc82f46245
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-02-18 17:11:52 +0200

    Class Record

--- main.py
+++ main.py
@@ -15,5 +15,23 @@ class Phone(Field):
     pass
 
 
+# Creating contacts
 class Record:
-    pass
+    
+    def __init__(self, name: Name, phone: Phone = None):
+        self.name = name
+        self.phones = []
+        if isinstance(phone, Phone):
+            self.phones.append(phone)
+
+    def add_phone(self, phone):
+        self.phone.append(phone)
+
+    def change_phone(self, phone):
+        self.phones = phone
+
+    def delete_phone(self):
+        self.phones = []
+
+    def show_contact(self):
+        return {"name": self.name, "phone": self.phones}
