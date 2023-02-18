21c9a522b3ee63d331cf94e73f6bb81ecaa0722a
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-02-18 17:01:00 +0200

    Classes

---
+++ main.py
@@ -0,0 +1,19 @@
+from collections import UserDict
+
+
+class Field:
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    pass
+
+
+class Record:
+    pass
